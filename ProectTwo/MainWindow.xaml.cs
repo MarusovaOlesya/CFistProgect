f87c91e4e09c54cc6feec8fa0bdacc59548be08e
marusova.ov <marusova.ov@3205EC12>
2023-10-18 11:54:47 +0300

    Добавьте файлы проекта.

---
+++ ProectTwo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace ProectTwo
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginInClick(object sender, RoutedEventArgs e)
+        {
+           
+            User user = new User();
+            user.Show();
+            this.Close();
+        }
+
+        private void SignUpClick(object sender, RoutedEventArgs e)
+        {
+            SignUp signUp = new SignUp();
+            signUp.Show();
+            this.Close();
+        }
+    }
+}
---
+++ ProectTwo/SignUp.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace ProectTwo
+{
+    /// <summary>
+    /// Логика взаимодействия для SignUp.xaml
+    /// </summary>
+    public partial class SignUp : Window
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+
+        private void RegisterClick(object sender, RoutedEventArgs e)
+        {
+            string fiobox = fioBox.Text;
+            string year = yearBox.Text;
+            string document = documentBox.Text;
+            User user = new User(fiobox, year, document);
+            user.Show();
+            this.Close();
+        }
+    }
+}
